6cd832af79456ab329df407dafe827942f30f37a
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-29 01:09:46 +0530

    [sai] Created class and created board method with size of array 10

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,6 +4,18 @@ public class TicTacToe {
 
    public static void main(String[] args) {
        System.out.println("Welcome to TicTacToe game");
+       char[] board = createBoard();
+
    }
+
+    public static char[] createBoard() {
+
+        char[] board = new char[10];
+        for(int index = 0; index < board.length; index++)
+        {
+            board[index] = ' ';
+        }
+        return  board;
+    }
 
 }
