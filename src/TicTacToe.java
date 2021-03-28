3ece25be524fade0b26d72a9b9810559078d7f38
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-29 01:24:49 +0530

    [sai] create method and take userinput and check the condition …

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,12 +1,32 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class TicTacToe {
 
    public static void main(String[] args) {
        System.out.println("Welcome to TicTacToe game");
        char[] board = createBoard();
+       char UserLetter = chooseLetter();
+       char ComputerLetter;
+       if(UserLetter=='X')
 
+           ComputerLetter='O';
+
+       else
+
+           ComputerLetter='X';
+
+       System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
    }
+
+    public static char chooseLetter(){
+        char symbol;
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Enter your symbol");
+        symbol=UserInput.next().toUpperCase().charAt(0);
+        return symbol;
+    }
+
 
     public static char[] createBoard() {
 
